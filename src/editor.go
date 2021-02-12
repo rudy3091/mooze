4bc3447d0170d5609ba6ebf410fefc5b8a65a1ad
RudyPark3091 <grayblack313@gmail.com>
2021-02-13 01:47:54 +0900

    Add cursor to editor
    TODO: changing cursor shape feature

--- src/editor.go
+++ src/editor.go
@@ -16,12 +16,13 @@ func NewMoozeEditor() *MoozeEditor {
 }
 
 func (e *MoozeEditor) readLine(m *mooze) string {
+	style := ToStyle("black", "green")
 	width, height := m.ms.Size()
 	w := NewMoozeWindow(5, 5, height-10, width-10, false)
 
 	w.Content([]string{"Ctrl + Enter to finish editor",
 		"(fn + Enter on macOS)"})
-	m.ms.RenderWindow(w, ToStyle("black", "green"))
+	m.ms.RenderWindow(w, style)
 	m.ms.Show()
 	w.Content([]string{})
 
@@ -33,8 +34,11 @@ func (e *MoozeEditor) readLine(m *mooze) string {
 
 	for {
 		if buf == "" {
-			m.ms.RenderWindow(w, ToStyle("black", "green"))
+			m.ms.RenderWindow(w, style)
 		}
+		m.ms.PrintInsideWindow(
+			w, e.CursorX, e.CursorY+2, "_", style,
+		)
 		ev := m.ms.EmitEvent()
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
@@ -42,12 +46,13 @@ func (e *MoozeEditor) readLine(m *mooze) string {
 
 		case *tcell.EventKey:
 			r := ev.Rune()
-			// Ctrl + Enter exits editor
 			switch r {
+			// Ctrl + Enter exits editor
 			case 10:
 				w.ContentAppend([]string{buf})
 				return e.Content
 
+			// enter
 			case 13:
 				w.ContentAppend([]string{buf})
 				buf = ""
@@ -56,26 +61,35 @@ func (e *MoozeEditor) readLine(m *mooze) string {
 				line += 1
 				e.CursorX += 1
 				e.CursorY = 5
-				m.ms.RenderWindow(w, ToStyle("black", "green"))
+				m.ms.RenderWindow(w, style)
+				m.ms.PrintInsideWindow(w, e.CursorX, e.CursorY+1, "_", style)
 				m.ms.Show()
 
+			// backspace
 			case 127:
 				if len(buf) > 0 {
 					e.Content = e.Content[0 : len(e.Content)-1]
 					buf = buf[0 : len(buf)-1]
 					m.ms.PrintInsideWindow(
-						w, e.CursorX, e.CursorY, " ", ToStyle("black", "green"),
+						w, e.CursorX, e.CursorY, "_", style,
+					)
+					m.ms.PrintInsideWindow(
+						w, e.CursorX, e.CursorY+1, " ", style,
+					)
+					m.ms.PrintInsideWindow(
+						w, e.CursorX, e.CursorY+2, " ", style,
 					)
 					e.CursorY -= 1
 				}
 				m.ms.Show()
 
+			// normal key input
 			default:
 				buf += string(r)
 				e.Content += string(r)
 				e.CursorY += 1
 				m.ms.PrintInsideWindow(
-					w, e.CursorX, e.CursorY, string(r), ToStyle("black", "green"),
+					w, e.CursorX, e.CursorY, string(r), style,
 				)
 				m.ms.Show()
 			}
