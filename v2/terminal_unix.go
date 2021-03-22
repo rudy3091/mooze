50e40bb3504513263cc736269e1cf42b37fbb949
RudyPark3091 <grayblack313@gmail.com>
2021-03-23 01:14:34 +0900

    Add terminal_unix.go
    terminal_unix is for terminal control in unix machine
    also gets input from user

---
+++ v2/terminal_unix.go
@@ -0,0 +1,54 @@
+package v2
+
+import (
+	"os"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+type TerminalUnix struct {
+	In    *os.File
+	State *terminal.State
+}
+
+func NewTerminalUnix() *TerminalUnix {
+	return &TerminalUnix{
+		In: openTty(),
+	}
+}
+
+func openTty() *os.File {
+	in, err := os.OpenFile("/dev/tty", syscall.O_RDONLY, 0)
+	if err != nil {
+		panic(err)
+	}
+	return in
+}
+
+func (t *TerminalUnix) MakeRaw() {
+	state, err := terminal.MakeRaw(int(t.In.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	t.State = state
+}
+
+func (t *TerminalUnix) RestoreRaw() {
+	terminal.Restore(int(t.In.Fd()), t.State)
+}
+
+func (t *TerminalUnix) MakeNonblock() {
+	err := syscall.SetNonblock(int(t.In.Fd()), true)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (t *TerminalUnix) RestoreNonblock() {
+}
+
+func (t *TerminalUnix) Read(buf []byte) []byte {
+	syscall.Read(int(t.In.Fd()), buf)
+	return buf
+}
