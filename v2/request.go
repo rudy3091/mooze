f1291c21fa11da38d2f7c9e11bec6ac4a82c653e
RudyPark3091 <grayblack313@gmail.com>
2021-03-23 23:21:12 +0900

    Add request.go
    request handles http operations
    currently fixed target url & method

---
+++ v2/request.go
@@ -0,0 +1,55 @@
+package v2
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+type httpMethodStruct struct {
+	GET  string
+	POST string
+}
+
+var HttpMethod = httpMethodStruct{
+	GET:  "GET",
+	POST: "POST",
+}
+
+type Request struct {
+	Url    string
+	Method string
+	Client *http.Client
+}
+
+func NewRequest() *Request {
+	r := &Request{
+		Url:    "",
+		Method: HttpMethod.GET,
+	}
+	r.SetHttpClient()
+	return r
+}
+
+func (r *Request) SetHttpClient() {
+	r.Client = &http.Client{}
+}
+
+func (r *Request) Send() []byte {
+	// req, err := http.NewRequest(HttpMethod.GET, r.Url, nil)
+	req, err := http.NewRequest("GET", "https://api.github.com/users/RudyPark3091", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := r.Client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	return body
+}
