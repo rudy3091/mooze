6c4f27afedc78a57edb1123adc2221bc762f0f7d
rudy3091 <grayblack313@gmail.com>
2021-06-11 01:11:24 +0900

    Add generator.go

---
+++ v3/args/generator.go
@@ -0,0 +1,35 @@
+package args
+
+type T string
+
+type ch chan T
+
+func FromStringSlice(s []string) []T {
+	t := []T{}
+	for _, str := range s {
+		t = append(t, T(str))
+	}
+	return t
+}
+
+func (c ch) Next() T {
+	return <-c
+}
+
+func Generate(source []T) ch {
+	yield := make(chan T)
+	idx := 0
+
+	go func() {
+		for {
+			if idx < len(source) {
+				yield <- source[idx]
+				idx++
+			} else {
+				yield <- ""
+			}
+		}
+	}()
+
+	return yield
+}
