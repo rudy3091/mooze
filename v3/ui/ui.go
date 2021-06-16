903a4ebb1cb758b9009575ada74ac517d6b5d070
rudy3091 <grayblack313@gmail.com>
2021-06-16 19:38:33 +0900

    Add keybinding messages

--- v3/ui/ui.go
+++ v3/ui/ui.go
@@ -11,3 +11,29 @@ func LoadAlternateScreen() {
 func UnloadAlternateScreen() {
 	fmt.Print("\033[?1049l")
 }
+
+func Print(s string) {
+	fmt.Print(s, "\r")
+}
+
+func Println(s string) {
+	fmt.Println(s, "\r")
+}
+
+func KeyBindings() {
+	main := Fg(
+		"Mooze: Yet another REST api test tool for command-line users",
+		MAGENTA)
+	Println(main)
+
+	Println("Request")
+	Println("- " + Fg("u", RED) + "rl: ")
+	Println("- " + Fg("m", RED) + "ethod: ")
+	Println("- " + Fg("b", RED) + "ody: ")
+	Println("- " + Fg("h", RED) + "eader: ")
+
+	Println("Operations")
+	Println("- " + Fg("r", RED) + "efresh screen")
+	Println("- " + Fg("s", RED) + "end request")
+	Println("- " + Fg("q", RED) + "uit")
+}
