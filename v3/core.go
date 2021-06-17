750afd98c50b0e10c6c9b8d90b531f04ad887efd
rudy3091 <grayblack313@gmail.com>
2021-06-17 19:34:40 +0900

    Add raw terminal functions

--- v3/core.go
+++ v3/core.go
@@ -1,15 +1,45 @@
 package v3
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"syscall"
 	"time"
 
 	"github.com/rudy3091/mooze/v3/ui"
+	"golang.org/x/term"
 )
+
+func openTty() *os.File {
+	in, err := os.OpenFile("/dev/tty", syscall.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal("cannot open /dev/tty")
+		panic(err)
+	}
+	return in
+}
+
+func makeRaw() (int, *term.State) {
+	fd := int(openTty().Fd())
+	state, err := term.MakeRaw(fd)
+	if err != nil {
+		log.Fatal("cannot make terminal raw")
+		panic(err)
+	}
+	return fd, state
+}
+
+func restoreRaw(fd int, s *term.State) {
+	term.Restore(fd, s)
+}
 
 func Run() {
 	ui.LoadAlternateScreen()
-	fmt.Println("Hello v3")
+	fd, state := makeRaw()
+
+	ui.KeyBindings()
 	time.Sleep(time.Second * 2)
+
+	restoreRaw(fd, state)
 	ui.UnloadAlternateScreen()
 }
