160c057afa8006436e24b897b84cfc2cce3525a4
rudy3091 <grayblack313@gmail.com>
2021-08-08 19:27:21 +0900

    Add window.focusNext/PrevPage method

--- v3/ui/manipulation.go
+++ v3/ui/manipulation.go
@@ -86,6 +86,18 @@ func (w *Window) focusPrev() {
 	}
 }
 
+func (w *Window) focusNextPage() {
+	if w.Meta.page < w.getPages() {
+		w.Meta.page += 1
+	}
+}
+
+func (w *Window) focusPrevPage() {
+	if w.Meta.page != 0 {
+		w.Meta.page -= 1
+	}
+}
+
 func NextItem() {
 	idx, err := getCurrentFocus()
 	if err != nil {
