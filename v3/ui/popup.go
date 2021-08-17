c3218e89b81c7dac12c7a338f7dac62d424fe464
rudy3091 <grayblack313@gmail.com>
2021-08-17 19:24:45 +0900

    Add popup.go
    handles popup window operations

---
+++ v3/ui/popup.go
@@ -0,0 +1,78 @@
+package ui
+
+import (
+	"io"
+	"os"
+
+	"golang.org/x/term"
+)
+
+var PopupWindow *Window
+var Input = false
+var inputTerm = term.NewTerminal(struct {
+	io.Reader
+	io.Writer
+}{os.Stdin, os.Stdout}, "> ")
+
+func OpenPopup(tw, th int) string {
+	width := tw / 2
+	height := th / 2
+
+	x := th / 4
+	y := tw / 4
+
+	// for i := 0; i < height-3; i++ {
+	// 	content = append(content, "")
+	// }
+	// content = append(content, "> _")
+
+	content := []string{}
+	PopupWindow = NewWindow(x, y, height, width).
+		Title("popup").
+		Content(content)
+
+	Input = true
+	defer func() { Input = false }()
+	defer ClosePopup()
+
+	PopupWindow.Clear()
+	PopupWindow.Render()
+	PopupWindow.Focus()
+
+	MoveCursorTo(x+height-2, y+1)
+	ShowCursor()
+	defer HideCursor()
+
+	line, err := inputTerm.ReadLine()
+	if err != nil {
+		// handle error
+	}
+	return line
+}
+
+func ClosePopup() {
+	PopupWindow.Close()
+	WindowStore[0].Focus()
+	ReloadAll()
+}
+
+func OpenPopupSelect(tw, th int) {
+	width := tw / 4
+
+	x := th/4 + 5
+	y := tw/4 + 10
+
+	PopupWindow = NewWindow(x, y, 10, width).
+		Title("select").
+		Content([]string{
+			"GET",
+			"POST",
+			"PUT",
+			"PATCH",
+			"DELETE",
+		})
+
+	PopupWindow.Focus()
+	PopupWindow.Clear()
+	PopupWindow.Render()
+}
